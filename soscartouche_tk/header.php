f2fe867b4c9b5b59920f4d57c5c11afcab0416e9
Alex <thereff10@hotmail.com>
2015-04-24 13:33:44 -0400

    Moar stuff

---
+++ soscartouche_tk/header.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 24/04/2015
+ * Time: 12:46 PM
+ */
+?>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/header.css" />
+</head>
+<header>
+    <img id="logo" alt="Logo" src="images/logo.png" height="200" />
+    <nav>
+        <ul>
+            <li><a href=#>Link 1</a></li>
+            <li><a href=#>Link 2</a></li>
+            <li><a href=#>Link 3</a></li>
+            <li><a href=#>Link 4</a></li>
+        </ul>
+    </nav>
+</header>
---
+++ soscartouche_tk/samplelayout.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>PAGE TITLE GOES HERE</title>
+        <!-- Every page will reference the mainFrame.css which contains the basic layout
+         of the page-->
+        <link rel="stylesheet" type="text/css" href="css/mainFrame.css" />
+        <link rel="stylesheet" type="text/css" href="css/fonts.css" />
+
+    </head>
+    <body>
+    <?php include "header.php"; ?>
+        <main>
+            <p>Main of page goes here</p>
+        </main>
+    </body>
+
+</html>
