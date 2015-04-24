5071ed778da87ea7da3e0ac1a2be1ad602ba67ab
Alex <thereff10@hotmail.com>
2015-04-24 09:44:19 -0400

    stuff
    gg

---
+++ includes/dbconnect.php
@@ -0,0 +1,8 @@
+<?php
+    $user_name = "yellow";
+    $password = "sysdev";
+    $database = "yellow_sysdev";
+    $server = "localhost";
+
+    $db_handle = mysql_connect($server, $user_name, $password);
+    $db_found = mysql_select_db($database, $db_handle);
